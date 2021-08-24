5f59a4ccefef8003c1dcd6569650936774c356ba
nelsonjcamacaro <nelsonjcamacaro@gmail.com>
2021-08-23 22:25:33 -0300

    creacion del header con barra de navegacion mediante el componente NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+
+
+
+
+
+function NavBar(){
+
+    return (
+        <header className="main-header">
+            <h1 className="main-h1">Browkies</h1>
+            <nav>
+                <ul className="ul-navbar">
+                    <li><a href="#" className="li-navbar">Inicio</a></li>
+                    <li><a href="#" className="li-navbar">Menu</a></li>
+                    <li><a href="#" className="li-navbar">Pedido</a></li>
+                </ul>
+            </nav>
+        </header>)
+}
+
+export default NavBar;
